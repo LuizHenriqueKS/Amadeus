01189047e5d496e0a4aef18c2c430c9d69a072de
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2021-01-12 21:40:22 -0300

    lerArquivoJson verifica se o arquivo existe

--- src/util/lerArquivoJson.js
+++ src/util/lerArquivoJson.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
+const ArquivoNaoEncontrado = require('../exception/ArquivoNaoEncontradoException');
 
-module.exports = function lerArquivoJson(arquivo) {
-  const conteudoArquivo = fs.readFileSync(arquivo);
+module.exports = function lerArquivoJson(caminhoArquivo) {
+  if (!fs.existsSync(caminhoArquivo)) {
+    throw new ArquivoNaoEncontrado(caminhoArquivo);
+  }
+  const conteudoArquivo = fs.readFileSync(caminhoArquivo);
   return JSON.parse(conteudoArquivo.toString());
 };
